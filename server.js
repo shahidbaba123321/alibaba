4954695d9fdcbf4961c80974d88de27ef4ef7ede
shahidbaba123321 <shahid.craftorbis@gmail.com>
2025-02-08 14:45:14 -0500

    Update server.js

--- server.js
+++ server.js
@@ -79,10 +79,7 @@ app.post('/register', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
 
 // API Endpoint for Payroll Metrics
 app.get('/api/payroll-metrics', async (req, res) => {
